d8d07aa0bc78b84f9e41d6f486243ee0ef1f161e
monqeth18-meet <ilybal@outlook.com>
2016-12-18 19:29:59 +0200

    added some new thing

--- UserAccount.py
+++ UserAccount.py
@@ -25,8 +25,8 @@ class UserAccount :
         #Complete this method - check the input variable, password_attempt,
         #against the password for this user (the value of the instance variable, password).
         #If, and only if, there's a match, then print the user's secret to the screen.
+        self.password_attemp = guess_password
         
-        
-        if password == self.password:
+        if guess_password == self.password:
             print('secret', password)
     
